bc4516a549d1cedbb3033fbf9f0ca30186e26ed8
Jonathon Hill <jhill9693@gmail.com>
2020-03-14 13:13:43 -0400

    Upgrade mongoose and other dependencies

--- test/_mongodb.js
+++ test/_mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.set('useCreateIndex', true);
 
 before(done => {
-    mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true }, err => {
+    mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true }, err => {
         if (err) return done(err);
         mongoose.connection.db.dropDatabase(done);
     });
